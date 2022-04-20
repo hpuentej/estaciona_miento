7d48dfb0aa2e011018944309ffa98db2fee5158a
HansPuente <hpuentej@uni.pe>
2022-04-20 03:46:46 -0500

    aun en proceso persistencia

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ let listaLugares = ['A1', 'B1', 'C1', 'D1', 'E1',
                     'A4', 'B4', 'C4', 'D4', 'E4',
                     'A5', 'B5', 'C5', 'D5', 'E5'];
 
-
-
-
 // Registrar tipo de estacionamiento
 
 function registrarTipos(tipos,listaLugares){
-  return setInterval(setCantidadTipos(tipos,listaLugares), 1000);  // cada 1 segundos
-}
-
-function conteo_Fijar_Places(tipos, listaLugares) {
-  const tiposX = cantidadLugares(tipos, listaLugares);
-  localStorage.setItem('tipos', JSON.stringify(tiposX));
+  // localStorage.
+  if (JSON.parse(localStorage.getItem('tipos')) != null){
+    const tiposX = JSON.parse(localStorage.getItem('tipos'));
+    return setInterval(setCantidadTipos(tiposX), 1000);  // cada 1 segundos
+  } else {
+    console.log('No habia datos en el localStorage');
+    cantidadLugares(tipos, listaLugares);
+    localStorage.setItem('tipos', JSON.stringify(tipos));
+    return setInterval(setCantidadTipos(tipos), 1000);  // cada 1 segundos    
+  }
 }
  
 function cantidadLugares(tipos, listaLugares) {
@@ -38,68 +39,50 @@ function cantidadLugares(tipos, listaLugares) {
   return tipos;                      
 }
 
-function setCantidadTipos(tipos, listaLugares) {
+function setCantidadTipos(tipos) {
   const valor_1 = document.querySelector(".val1");
   const valor_2 = document.querySelector(".val2");
   const valor_3 = document.querySelector(".val3");
   const valor_4 = document.querySelector(".val4");
-
-  // if(localStorage.getItem('tipos') != null) {
-  //   const tipos = JSON.parse(localStorage.getItem('tipos'));
-  //   conteo_Fijar_Places(tipos, listaLugares);   
-
-  //   valor_1.innerText = `${tipos.disponible}` 
-  //   valor_2.innerText = `${tipos.reservado}`
-  //   valor_3.innerText = `${tipos.ocupado}` 
-  //   valor_4.innerText = `${tipos.noDisponible}` 
-  // } else {
-  //   conteo_Fijar_Places(tipos, listaLugares);   
-  //   const tiposZ = JSON.parse(localStorage.getItem('tipos'));
-  //   valor_1.innerText = `${tiposZ.disponible}` 
-  //   valor_2.innerText = `${tiposZ.reservado}`
-  //   valor_3.innerText = `${tiposZ.ocupado}` 
-  //   valor_4.innerText = `${tiposZ.noDisponible}` 
-  //   }
-
-  conteo_Fijar_Places(tipos, listaLugares);   
-  const tiposZ = JSON.parse(localStorage.getItem('tipos'));
-  valor_1.innerText = `${tiposZ.disponible}` 
-  valor_2.innerText = `${tiposZ.reservado}`
-  valor_3.innerText = `${tiposZ.ocupado}` 
-  valor_4.innerText = `${tiposZ.noDisponible}` 
+  
+  valor_1.innerText = `${tipos.disponible}` 
+  valor_2.innerText = `${tipos.reservado}`
+  valor_3.innerText = `${tipos.ocupado}` 
+  valor_4.innerText = `${tipos.noDisponible}` 
 }
 
 // Fijar color de estacionamiento
 
 function onClickButtonElegir(tipos, listaLugares) {
-
   const inputLugar = document.querySelector(".inputLugar");
   const lugarValue = inputLugar.value;
 
   const inputTipo = document.querySelector(".inputTipo");
   const tipoValue = inputTipo.value;
 
-  const lugar = document.querySelector(`.esta${lugarValue}`);  
-
-  // if (tipoValue != '') {
-  //   lugar.className = `esta${lugarValue} ${tipoValue}`;      
-  // }
-
-  // const confirmar = document.querySelector(".confirmar"); 
-  // confirmar.innerText = `Ultimo lugar actualizado: Lugar ${lugarValue} tiene el estatus de ${tipoValue}`;
   const tiposX = Object.keys(tipos);
   list = [];
 
-  tiposX.forEach(tipo => {
-    if (tipoValue === tipo  && tipoValue != '') {
-      list.push(true);
-    } else {
-      list.push(false);
+  for (let tipo of tiposX){
+    for (let lugarX of listaLugares) {
+      if (tipoValue === tipo  && lugarValue === lugarX) {
+        list.push(true);        
+      } else {
+        list.push(false);
+      }
     }
-  });
-
-  if (list.includes(true)) { 
-    lugar.className = `esta${lugarValue} ${tipoValue}`; // Cambiar clase y pintar el lugar     
+  };
+  
+  if (list.includes(true)) {
+    const lugar = document.querySelector(`.esta${lugarValue}`); 
+    // localStorage.setItem('lugar', JSON.stringify(lugar));
+    // const lugarY = JSON.parse(localStorage.getItem('lugar'));     
+    lugar.className = `esta${lugarValue} ${tipoValue}`; // Cambiar clase y pintar el lugar
+    const lugarY = lugar.className;
+    localStorage.setItem('lugar', JSON.stringify(lugarY));
+    const lugarZ = JSON.parse(localStorage.getItem('lugar'));
+    lugarZ.className = `esta${lugarValue} ${tipoValue}`;
+    
     const confirmar = document.querySelector(".confirmar"); 
     confirmar.innerText = `Ultimo lugar actualizado: El lugar ${lugarValue} tiene el estado ${tipoValue}`;
   } else {
@@ -107,6 +90,8 @@ function onClickButtonElegir(tipos, listaLugares) {
     confirmar.innerText = `El lugar ${lugarValue} no existe o no selecciono un tipo de estacionamiento adecuado`;
   }
 }
+
+
 
 
 
